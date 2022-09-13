8072fed9a923070e2e2ea0778d490f0624e06ebc
Atanas Kodzhamanov <39922895+AtanasKodzhamanov@users.noreply.github.com>
2022-09-13 18:48:45 +0100

    Update settings.py

--- DogShelter/settings.py
+++ DogShelter/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-iax8&wb&pl^j%u1$t58*#nsjn6_75th$s)5#fj#t^tg!@l1kp-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
