973e11a335bf94e97d59eff467a9ae1ede3af67d
AtanasLinux <kodzhamanov@gmail.com>
2022-09-17 08:20:08 +0100

    Dog Model

--- DogShelter/web/admin.py
+++ DogShelter/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
+from DogShelter.web.models import Dog
 # Register your models here.
+
+
+class PetInlineAdmin(admin.StackedInline):
+    model = Dog
+
+
+@admin.register(Dog)
+class Dog(admin.ModelAdmin):
+    pass
--- DogShelter/web/models.py
+++ DogShelter/web/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+
+class Dog(models.Model):
+
+    NAME_MAX_LENGTH = 60
+
+    Options = [(x, x) for x in ("Y","N", "Unknown")]
+
+    # Fields(Columns)
+    nameENG = models.CharField(
+        max_length=NAME_MAX_LENGTH
+    )
+    nameBG = models.CharField(
+        max_length=NAME_MAX_LENGTH
+    )
+
+    monthly_upkeep = models.IntegerField()
+
+    diseases= models.CharField(
+        max_length=7,
+        choices=Options
+    )
+
+    gender= models.CharField(
+        max_length=7,
+        choices=Options
+    )
+
+    profile_pic = models.URLField(
+        max_length = 300
+    )
+
+    virtual_adopter= models.CharField(
+        max_length=NAME_MAX_LENGTH,
+    )
+
+    arrival_date = models.DateTimeField(
+    )
+
+    story_ENG = models.TextField(
+        null=True,
+        blank=True,
+    )
+    story_BG = models.TextField(
+        null=True,
+        blank=True,
+    )
+
