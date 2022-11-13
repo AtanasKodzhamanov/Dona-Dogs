417e10b4d42cd19f18f70df2437fcf0d61222b57
Atanas <kodzhamanov@gmail.com>
2022-11-13 18:48:37 +0000

    Add pop up when hover

--- staticfiles/static/js/javascript.js
+++ staticfiles/static/js/javascript.js
@@ -1,4 +1,11 @@
-function myFunction() {
-    alert("Hello from a static file!");
-  }
-  
+function myFunction1(element) {
+  var popup = element.querySelector('#myPopup');
+  popup.classList.toggle("show", true);
+
+} 
+
+function myFunction2(element) {
+  var popup = element.querySelector('#myPopup');
+  popup.classList.toggle("show", false);
+
+} 
