5193463909ee03a3919d480362485f1e3809725f
Atanas <kodzhamanov@gmail.com>
2022-11-15 04:25:51 +0000

    collect static

--- staticfiles/js/javascript.js
+++ staticfiles/js/javascript.js
@@ -2,7 +2,7 @@ function myFunction1(element) {
   var popup = element.querySelector('#myPopup');
   popup.classList.toggle("show", true);
 
-} 
+}
 
 function myFunction2(element) {
   var popup = element.querySelector('#myPopup');
