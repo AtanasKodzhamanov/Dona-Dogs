0a00ca4aafef3b6db7328baa1a02e605da8acbad
Atanas <kodzhamanov@gmail.com>
2022-12-17 22:53:33 +0000

    Test active nav in production

---
+++ staticfiles/static/js/activeNav.js
@@ -0,0 +1,14 @@
+function setActiveNavItem(item) {
+    // Get all nav items
+    const navItems = document.querySelectorAll('li.w3-bar-item');
+    
+    // Remove the 'w3-bottombar' class from all nav items
+    navItems.forEach(navItem => navItem.classList.remove('w3-bottombar'));
+    
+    // Add the 'w3-bottombar' class to the clicked nav item
+    item.classList.add('w3-bottombar');
+  }
+  
+  const navItems = document.querySelectorAll('li.w3-bar-item');
+  navItems.forEach(navItem => navItem.addEventListener('click', () => setActiveNavItem(navItem)));
+  
