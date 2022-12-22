e99947a669418cfeda094e270c4397f1609b52fb
atanas <kodzhamanov@gmail.com>
2022-12-23 01:44:18 +0200

    Test MIME error

--- staticfiles/static/js/redirectToDogAlbum.js
+++ staticfiles/static/js/redirectToDogAlbum.js
@@ -1,15 +1,9 @@
-// Get all card elements on the page
-const dogCards = document.querySelectorAll('#Dog');
+const dogCards = document.querySelectorAll('#Dog')
 
-// Add a click event listener to each card
 dogCards.forEach((card) => {
   card.addEventListener('click', () => {
-    // Have cursor change to a pointer when hovering over a card
-    card.style.cursor = 'pointer';
-    // Get the primary key value from the card's "pk" attribute
-    const pk = card.getAttribute('pk');
-    // Redirect to the /dogs/pk URL
-
-    window.location.href = `/dogs/${pk}`;
-  });
-});
+    card.style.cursor = 'pointer'
+    const pk = card.getAttribute('pk')
+    window.location.href = `/dogs/${pk}`
+  })
+})
