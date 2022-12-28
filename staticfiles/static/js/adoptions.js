516b81943009ad7bfa8810499c7d2e689a01ae3f
atanas <kodzhamanov@gmail.com>
2022-12-28 14:12:31 +0200

    Expand on click

--- staticfiles/static/js/adoptions.js
+++ staticfiles/static/js/adoptions.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const triggers = document.querySelectorAll('#AdoptionExpand');
     triggers.forEach(trigger => {
-      trigger.addEventListener('mouseover', function() {
+      trigger.addEventListener('click', function() {
         const adoptionCards = this.querySelectorAll('#AdoptionCard');
         adoptionCards.forEach(adoptionCard => {
           if (adoptionCard.style.display === 'none') {
             adoptionCard.style.display = 'block';
-          } 
+          } else {
+            adoptionCard.style.display = 'none';
+          }
         });
       });
     });
   });
+  
   
   
