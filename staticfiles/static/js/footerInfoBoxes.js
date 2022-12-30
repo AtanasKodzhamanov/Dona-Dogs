a81d76895f98948ce7767b0d69e8b97e30a91ca4
atanas <kodzhamanov@gmail.com>
2022-12-30 13:56:37 +0200

    Update formats and fix PayPal bug

--- staticfiles/static/js/footerInfoBoxes.js
+++ staticfiles/static/js/footerInfoBoxes.js
@@ -47,7 +47,11 @@ const box3 = document.querySelector('.box3');
 box3.addEventListener('mouseover', function () {
   hideVA.style.display = 'none';
   hideSubscribeForm.style.display = 'none';
-  hideDonations.style.display = 'flex';
+  if (window.innerWidth < 600){
+  hideDonations.style.display = 'block';}
+  else{
+    hideDonations.style.display = 'flex';
+  }
 
   hideVA.style.opacity = 0;
   hideSubscribeForm.style.opacity = 0;
