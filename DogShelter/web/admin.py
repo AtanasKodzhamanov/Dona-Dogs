1a345c9f5d2962ceebd25d047bdfa30ce6517f9a
Atanas <kodzhamanov@gmail.com>
2023-01-18 03:23:59 +0000

    Add ID to admin panel

--- DogShelter/web/admin.py
+++ DogShelter/web/admin.py
@@ -14,36 +14,41 @@ class PetInlineAdmin(admin.StackedInline):
 
 
 @admin.register(Dog)
-class Dog(admin.ModelAdmin):
+class DogAdmin(admin.ModelAdmin):
     list_display = ["name_eng", "id", "status", "va_name_eng", "profile_pic", "pic_2", "pic_3",
                     "pic_4", "pic_5", "adoption_pic_after_1", "adoption_pic_after_2", "adoption_pic_after_3"]
     list_filter = ["status", ("story_bg", admin.EmptyFieldListFilter),
                    ("story_eng", admin.EmptyFieldListFilter)]
+    list_display_links = ["id"]
 
 
 @admin.register(NoticeBoard)
-class NoticeBoard(admin.ModelAdmin):
+class NoticeBoardAdmin(admin.ModelAdmin):
     list_display = ["location", "note_bg", "note_pic_1", "note_pic_2"]
     list_display_links = ["note_pic_1", "note_pic_2"]
 
 
 @admin.register(About)
-class About(admin.ModelAdmin):
+class AboutAdmin(admin.ModelAdmin):
     list_display = ["section_title_bg", "section_title_eng"]
+    list_display_links = ["id"]
     readonly_fields = ('id',)
 
 
 @admin.register(Donation)
-class Donation(admin.ModelAdmin):
+class DonationAdmin(admin.ModelAdmin):
     list_display = ["person_name_eng", "date"]
+    list_display_links = ["id"]
     list_per_page = 100
 
 
 @admin.register(DonationStory)
-class DonationStory(admin.ModelAdmin):
+class DonationStoryAdmin(admin.ModelAdmin):
     list_display = ["donation_text_bg", "donation_text_eng", "date"]
+    list_display_links = ["id"]
 
 
 @admin.register(NewsletterSubscriber)
-class NewsletterSubscriber(admin.ModelAdmin):
+class NewsletterSubscriberAdmin(admin.ModelAdmin):
     list_display = ["email", "name"]
+    list_display_links = ["id"]
