a37cd61916f395b9f1462c5e6474062bb82073dc
Atanas <kodzhamanov@gmail.com>
2023-02-24 14:11:16 +0000

    Set allowed hosts

--- DogShelter/settings.py
+++ DogShelter/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['www.donadogs.org', 'www.donadogs.com', '127.0.0.1']
 
 
 # Application definition
