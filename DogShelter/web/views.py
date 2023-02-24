9c36af1c9e27b17e7a0fe45dae6ef3a592347efb
Atanas <kodzhamanov@gmail.com>
2023-02-24 08:29:31 +0000

    Subscirber form fix and template rearrangement

--- DogShelter/web/views.py
+++ DogShelter/web/views.py
@@ -1,24 +1,30 @@
 import datetime
 import json
+from django import template
 from django.core import serializers
-from django.forms.models import model_to_dict
+from django.http import HttpResponse
 from django.views.generic import DetailView, ListView, TemplateView
 from DogShelter.web.forms import SubscribeForm, DogFilterForm, vaStatusForm, genderFilterForm
 from DogShelter.web.models import Dog, DonationStory, NoticeBoard, LongPost, Donation
-
+from django.shortcuts import render, redirect
 # responsible for the subscribe form in the footnote of every page
 
 # ----- Utility functions -----
 
 
 def renderCommon(request):
-    if request.method == "GET":
-        subscribeForm = SubscribeForm()
-    else:
-        subscribeForm = SubscribeForm(request.POST or None)
-        if subscribeForm.is_valid():
-            subscribeForm.save()
+    subscribeForm = SubscribeForm(request.POST or None)
     return subscribeForm
+
+
+def handle_form_post(request, template_name=None, context=None):
+    form = SubscribeForm(request.POST)
+    if form.is_valid():
+        form.save()
+
+    context = context or {}
+    context.update({'subscribe_form': form})
+    return render(request, template_name, context=context)
 
 # ----- Views -----
 
@@ -42,9 +48,6 @@ class BaseView(TemplateView):
         context['content_containter_pics'] = [
             f'about_pic_{i}' for i in range(1, 11)]
 
-        # get the subscribe form for the footer
-        context['subscribe_form'] = renderCommon(self.request)
-
         return context
 
     def get_context_data(self, **kwargs):
@@ -60,7 +63,6 @@ class HomeView(BaseView):
         context = super().get_common_context_data()
 
         # get all dogs from the database
-
         data_dogs = Dog.objects.filter(
             status__in=["Active", "Sick", "New"]).order_by("?")
         data_dogs = data_dogs[:6]
@@ -98,14 +100,25 @@ class HomeView(BaseView):
 
             # date for the donations slideshow
             "date_pk": date_pk,
+
+            # subscribe form
+            "subscribe_form": SubscribeForm(self.request.POST or None),
         })
 
         return context
 
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='home.html',
+            context=self.get_common_context_data()
+        )
+
+
 # responsible for showing the monthly donation summary pages
 
 
-class DonationStoryView(ListView):
+class DonationStoryView(TemplateView):
     model = DonationStory
     template_name = "donationMonthly.html"
 
@@ -134,8 +147,15 @@ class DonationStoryView(ListView):
         context["date"] = date
 
         # common context data
-        context['subscribe_form'] = renderCommon(self.request)
+        context['subscribe_form'] = SubscribeForm(self.request.POST or None)
         return context
+
+    def post(self, request, *args, **kwargs):
+        return handle_form_post(request, template_name=self.template_name, context={
+            'date_pk': self.kwargs['date_pk'],
+            **self.get_context_data()
+        })
+
 
 # responsible for showing the individual dog profile pages
 
@@ -164,8 +184,17 @@ class DogProfileView(DetailView):
         context['active_dog_ids'] = json.dumps(active_dog_ids)
 
         # common context data
-        context['subscribe_form'] = renderCommon(self.request)
+        context['subscribe_form'] = SubscribeForm(self.request.POST or None)
         return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return handle_form_post(request, template_name=self.template_name, context={
+            'pk': self.kwargs['pk'],
+            'object': self.object,  # Add the object to the context dictionary
+            **self.get_context_data(**kwargs)
+        })
+
 
 # responsible for rendering the gallery page
 
@@ -188,8 +217,16 @@ class GalleryView(BaseView):
             status__in=["Active", "Sick"]).order_by("arrival_year")
         context['data_people'] = set(Dog.objects.values_list(
             "va_name_bg", "va_name_eng").exclude(va_name_eng=""))
+        context['subscribe_form'] = SubscribeForm(self.request.POST or None)
 
         return context
+
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='dogGallery.html',
+            context=self.get_common_context_data()
+        )
 
 
 # responsible for rendering the clinic page
@@ -205,7 +242,8 @@ class ClinicView(BaseView):
         data_dogs = Dog.objects.filter(status="Sick").order_by("?")
 
         context.update({
-            "data_dogs": data_dogs
+            "data_dogs": data_dogs,
+            "subscribe_form": SubscribeForm(self.request.POST or None),
         })
 
         return context
@@ -215,6 +253,13 @@ class ClinicView(BaseView):
 
         return self.render_to_response(context)
 
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='clinic.html',
+            context=self.get_common_context_data()
+        )
+
 
 # renders the about page
 
@@ -223,8 +268,18 @@ class AboutView(BaseView):
 
     def get_common_context_data(self):
         context = super().get_common_context_data()
+        context.update({
+            "subscribe_form": SubscribeForm(self.request.POST or None),
+        })
 
         return context
+
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='ourstory.html',
+            context=self.get_common_context_data()
+        )
 
 
 # renders the adoption page
@@ -248,9 +303,17 @@ class AdoptionsView(BaseView):
             "data_dogs": data_dogs,
             "dogs": dogs,
             "years": years,
-            "field_names": field_names
+            "field_names": field_names,
+            "subscribe_form": SubscribeForm(self.request.POST or None)
         })
         return context
+
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='adoptions.html',
+            context=self.get_common_context_data()
+        )
 
 
 # renders the donations page
@@ -276,7 +339,15 @@ class DonationsView(BaseView):
             "data_people": data_people,
             "last_month": last_month,
             "month": last_month_cl,
-            "data_donations": data_donations
+            "data_donations": data_donations,
+            "subscribe_form": SubscribeForm(self.request.POST or None)
         })
 
         return context
+
+    def post(self, request):
+        return handle_form_post(
+            request,
+            template_name='donations.html',
+            context=self.get_common_context_data()
+        )
