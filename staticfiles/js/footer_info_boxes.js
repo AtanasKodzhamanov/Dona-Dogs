1962fb18a36a5cf3a78e8a744bc2a8fe4ef3810c
Atanas <kodzhamanov@gmail.com>
2023-06-11 00:36:45 +0100

    fix footer JS, add zoom

--- staticfiles/js/footer_info_boxes.js
+++ staticfiles/js/footer_info_boxes.js
@@ -1,35 +1,36 @@
 // This code is responsible for showing the footer info boxes on the home page when the mouse hovers over them.
 
 // Footer subscribe form
-document
+/* document
   .getElementById("footer-subscribe-form")
   .addEventListener("submit", function (event) {
     document.getElementById("footer-subscribe-form").style.display = "none";
     document.getElementById("success-message").style.display = "block";
   });
-
+*/
 // Virtual adoption container
 const hideVA = document.querySelector(".hideVA");
-const hideSubscribeForm = document.querySelector(".subscribe-form-hide");
+// const hideSubscribeForm = document.querySelector(".subscribe-form-hide");
 const hideDonations = document.querySelector(".hide-donations-box");
 
 hideVA.style.opacity = 0;
-hideSubscribeForm.style.opacity = 0;
+// hideSubscribeForm.style.opacity = 0;
 hideDonations.style.opacity = 0;
 
 const box1 = document.querySelector(".box1");
 box1.addEventListener("mouseover", function () {
   hideVA.style.display = "block";
-  hideSubscribeForm.style.display = "none";
+  // hideSubscribeForm.style.display = "none";
   hideDonations.style.display = "none";
 
-  hideSubscribeForm.style.opacity = 0;
+  // hideSubscribeForm.style.opacity = 0;
   hideDonations.style.opacity = 0;
   setTimeout(function () {
     hideVA.style.opacity = 1;
   }, 100);
 });
 
+/*
 const box2 = document.querySelector(".box2");
 box2.addEventListener("mouseover", function () {
   hideVA.style.display = "none";
@@ -43,11 +44,11 @@ box2.addEventListener("mouseover", function () {
     hideSubscribeForm.style.opacity = 1;
   }, 100);
 });
-
+*/
 const box3 = document.querySelector(".box3");
 box3.addEventListener("mouseover", function () {
   hideVA.style.display = "none";
-  hideSubscribeForm.style.display = "none";
+  // hideSubscribeForm.style.display = "none";
   if (window.innerWidth < 600) {
     hideDonations.style.display = "block";
   } else {
@@ -55,7 +56,7 @@ box3.addEventListener("mouseover", function () {
   }
 
   hideVA.style.opacity = 0;
-  hideSubscribeForm.style.opacity = 0;
+  // hideSubscribeForm.style.opacity = 0;
 
   setTimeout(function () {
     hideDonations.style.opacity = 1;
